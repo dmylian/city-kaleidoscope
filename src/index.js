b2e01cb886e8e69cdb9bb2dd5e2657f6e0c7b8ba
Danylo Mylian <danylo.ukl@gmail.com>
2020-12-29 23:15:20 +0200

    fix: remove dep

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'mapbox-gl/dist/mapbox-gl.css';
 import 'tachyons/css/tachyons.css';
 
 ReactDOM.render(
